344cbd6c4e883daf5b1cd18ad6c21420edce0d12
leumast <2178768@cstj.qc.ca>
2023-11-08 17:49:43 -0500

    planification des classes

--- tp3_prog/tp3_prog/App.xaml.cs
+++ tp3_prog/tp3_prog/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 
 namespace tp3_prog
@@ -13,5 +8,39 @@ namespace tp3_prog
     /// </summary>
     public partial class App : Application
     {
+        public class Npc
+        {
+            public int Name { get; set; }
+        }
+        public class Enemy
+        {
+            public string Name { get; set; }
+
+        }
+        public class Hero
+        {
+            public string Name { get; set; }
+            public Classe Classe { get; set; }
+            public int Health { get; set; }
+            public int Attack { get; set; }
+            public int Defense { get; set; }
+            public int MagicPoints { get; set; }
+
+            public int Experience { get; set; }
+            public int Level { get; set; }
+        }
+
+        public class Classe
+        {
+            public string Portrait { get; set; }
+            public List<string> Magic { get; set; }
+
+        }
+
+        public class Zone
+        {
+            public string Name { get; set; }
+            public int ChanceOfCombat { get; set; }
+        }
     }
 }
