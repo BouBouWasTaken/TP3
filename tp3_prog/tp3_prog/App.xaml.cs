b66f8e78de694b8bfcceeca97729ecc28eda4017
leumast <2178768@cstj.qc.ca>
2023-11-13 16:23:12 -0500

    Suite des Classes

--- tp3_prog/tp3_prog/App.xaml.cs
+++ tp3_prog/tp3_prog/App.xaml.cs
@@ -10,11 +10,22 @@ namespace tp3_prog
     {
         public class Npc
         {
-            public int Name { get; set; }
+            public int Id { get; set; }
+            public string Name { get; set; }
         }
         public class Enemy
         {
+            public int Id { get; set; }
             public string Name { get; set; }
+
+        }
+        public class Crafter
+        {
+            public List<string> DialogOptions { get; set; }
+            public List<string> Inventory { get; set; }
+            public List<string> Ingredients { get; set; }
+            public List<string> Items { get; set; }
+            public List<string> Recipies { get; set; }
 
         }
         public class Hero
@@ -29,6 +40,10 @@ namespace tp3_prog
             public int Defense { get; set; }
 
         }
+        public class Merchant
+        {
+
+        }
 
         public class Classe
         {
@@ -38,7 +53,7 @@ namespace tp3_prog
             public int DefByLevel { get; set; }
             public int HpByLevel { get; set; }
             public int MpByLevel { get; set; }
-            public List<Magic> Magic { get; set; }
+            public List<Magic> Magics { get; set; }
         }
         public class Magic
         {
@@ -48,15 +63,87 @@ namespace tp3_prog
             public string Targets { get; set; }
             public int AtkMultip { get; set; }
 
-
-
         }
 
         public class Zone
         {
             public string Name { get; set; }
             public int ChanceOfCombat { get; set; }
+            public List<Zone> Zones { get; set; }
         }
 
+        public class Fight
+        {
+            public List<Enemy> Enemies { get; set; }
+            public int Rounds { get; set; }
+            public bool Finished { get; set; }
+            public string type { set; get; }
+        }
+
+        public class Auberge
+        {
+            public int Rooms { get; set; }
+            public int Cost { get; set; }
+            public int LifeRegen { get; set; }
+            public int MpRegen { get; set; }
+
+        }
+        public class Interaction
+        {
+            public List<string> DialogOptions { get; set; }
+            public List<string> CombatOptions { get; set; }
+        }
+
+        // Items
+
+        public class Item
+        {
+
+        }
+        public class Equipment : Item
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Price { get; set; }
+            public int Slot { get; set; }
+            public List<Hero> Users { get; set; }
+            public int ItemValue { get; set; }
+        }
+
+        public class Usable : Item
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Price { get; set; }
+            public string EffectType { get; set; }
+            public int Target { get; set; }
+            public int EffectValue { get; set; }
+        }
+        public class Component : Item
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Price { get; set; }
+        }
+
+        public class Sign
+        {
+            public List<Zone> ZonesConnected { get; set; }
+            public List<Zone> ZonesVisited { get; set; }
+
+            public List<Zone> ZonesNotVisited { get; set; }
+            public RestingArea RestArea { get; set; }
+
+        }
+        public class RestingArea
+        {
+            public int Id { get; set; }
+            public int Regen { get; set; }
+            public int ChanceOfCombat { get; set; }
+
+
+        }
     }
+
+
 }
