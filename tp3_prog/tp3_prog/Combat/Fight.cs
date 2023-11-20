0b4827ce4354a1dba662309f11f965d6029709f3
BouBouWasTaken <2048654@cstj.qc.ca>
2023-11-19 23:58:36 -0500

    Added the classes for Combat
    Might change in the future, don't like how I have two different classes that will handle the same behavior.

---
+++ tp3_prog/tp3_prog/Combat/Fight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace tp3_prog.Combat
+{
+    internal class Fight
+    {
+        // When a fight breaks out, this is called and generates it.
+
+        public Fight(Party heroes)
+        {
+            Heroes = heroes;
+            Enemies = generateEnemies();
+        }
+
+        // Function that will make the rounds one after the other and concludes when it's done
+
+        public EnemyGroup generateEnemies()
+        {
+            List<Enemy> newList = new List<Enemy>();
+
+            EnemyGroup enemies = new EnemyGroup(newList);
+
+            return enemies;
+        }
+
+        public Party Heroes { get; set; }
+
+        public EnemyGroup Enemies { get; set; }
+    }
+}
---
+++ tp3_prog/tp3_prog/Combat/Round.cs
@@ -0,0 +1,18 @@
+﻿namespace tp3_prog.Combat
+{
+    internal class Round : Fight
+    {
+        public Round(Party heroes, EnemyGroup enemies) : base(heroes)
+        {
+            Heroes_Party = heroes;
+            Enemies_Party = enemies;
+        }
+
+        public Party Heroes_Party { get; set; }
+        public EnemyGroup Enemies_Party { get; set; }
+
+        // Fonctions that will deal with everything around the combat system,
+        // Using the buttons, you can attack and the function will be here.
+
+    }
+}
