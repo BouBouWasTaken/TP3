173d39efe8e2a1156f20560141d2d2bab78dde24
Olivier Vallée <2048654@cstj.qc.ca>
2023-12-05 17:30:17 -0500

    Second commit
    I didn't save all, ignore this commit.

--- tp3_prog/tp3_prog/Classes/Items/Item.cs
+++ tp3_prog/tp3_prog/Classes/Items/Item.cs
@@ -6,7 +6,6 @@ namespace tp3_prog
         public int Id { get; set; } // 3201832
         public string? Name { get; set; } // X's sword
         public int Price { get; set; } // 5 Golds
-        public int Amount { get; set; }
         public string? Description { get; set; }
         public Hero? CurrentOwner { get; set; } // Who owns it
         public int Value { get; set; }
@@ -20,12 +19,6 @@ namespace tp3_prog
         {
 
         }
-
-        public override string ToString()
-        {
-            return $"{Name} ({Amount})";
-        }
-
     }
 
 
